45c37f58d7d3d6c4fc794efabb53653b204a9990
ToX82 <toss82@gmail.com>
2014-06-17 16:07:14 +0200

    Update index.php
    Added whitelist and blacklist configurations and behavior

--- index.php
+++ index.php
@@ -7,20 +7,16 @@
  * Copyright (c) 2014, Emanuele "ToX" Toscano
  * Available via the MIT license.
  * see: https://github.com/ToX82/cakephp-supercache for details
- * index.php - version. 0.1
+ * index.php - version. 0.2
  *
  *
  * INSTALLATION STEPS:
  * 1) create a new folder called "cache" in your app/webroot directory, with write permissions
  * 2) rename app/webroot/index.php to app/webroot/index_cake.php
  * 3) save this file to app/webroot. Yes, you're replacing the original file with this one
- * 4) that's all folks!
+ * 4) configure as you need
+ * 5) that's all folks!
  */
-
-session_name('CAKEPHP');
-session_start();
-$timerStart = microtime(true);
-
 
 
 /*
@@ -30,49 +26,93 @@ $timerStart = microtime(true);
 */
 $cache = 1;
 $debug = 0;
+$filterType = "blacklist";  // "whitelist" caches specified pages - caches nothing by default
+                            // "blacklist" don't cache specified pages - caches everything by default
 
-$cachePath = getcwd().'/cache'; // the path of cache folder, starting from app/webroot
+$cacheHomePage = true;      // whether or not we want to cache the home page (you may need it if filterType is whitelist and you want the homepage to be cached)
 
+$whitelist = array(
+    "/pages"
+);
+$blacklist = array(
+    "/login",
+    "/users",
+    "/contacts"
+);
 
 
 /*
+* CAKEPHP RELATED CONFIGURATION
+*/
+session_name('CAKEPHP');
+session_start();
+$timerStart = microtime(true);
+$cachePath = getcwd().'/cache'; // the path of cache folder, starting from app/webroot
+
+/*
 *
-* LET'S DO SOME CHECKS TO SEE IF WE NEED A CACHE FILE OR NOT
-* p.s. you have to put your cacheable uris here :)
+* CHECK IF WE NEED A CACHE FILE OR NOT
 *
 */
 
-// PATH BASED CHECK
-$homepath = str_replace($_SERVER['DOCUMENT_ROOT'], "", $_SERVER['SCRIPT_FILENAME']);
-$homepath = str_replace("app/webroot/index.php", "", $homepath); // root path. This is to make it caching work even if cakephp is installed in a sub-folder
+// PATH BASED CHECKS
+// based on whitelist, blacklist and cacheHomePage configurations
 
-if ($cache == 1 and (
-        $_SERVER["REQUEST_URI"] == $homepath // cache the home page...
-         or 
-        strpos($_SERVER["REQUEST_URI"], "pages/") > 0 // ... everything from pages controller...
-         or 
-        strpos($_SERVER["REQUEST_URI"], "news/") > 0 // ... and from news controller
-    )) {
+if ($cache == 0) {
+    $debugInfo = "Caching disabled...";
 } else {
-    $debugInfo = "Not a cacheable page";
-    $cache = 0;
+    switch ($filterType) {
+        case "whitelist":
+            $cache = 0;
+            foreach ($whitelist as $word) {
+                if (strpos($_SERVER["REQUEST_URI"], $word) > -1) {
+                    $debugInfo = "Whitelisted page, cache it!";
+                    $cache = 1;
+                }
+            }
+            break;
+        case "blacklist":
+            $cache = 1;
+            foreach ($blacklist as $word) {
+                if (strpos($_SERVER["REQUEST_URI"], $word) > -1) {
+                    $debugInfo = "Blacklisted page! I'm not caching it";
+                    $cache = 0;
+                }
+            }
+            break;
+    }
+    if ($cacheHomePage == true) {
+        $homepath = str_replace($_SERVER['DOCUMENT_ROOT'], "", $_SERVER['SCRIPT_FILENAME']);
+        $homepath = str_replace("app/webroot/index.php", "", $homepath); // root path. This is to make it caching work even if cakephp is installed in a sub-folder
+
+        if ($_SERVER["REQUEST_URI"] == $homepath) {
+            $debugInfo = "Home page, cache it!";
+            $cache = 1;
+        }
+    }
 }
 
-// AUTHENTICATION BASED CHECK 
-// ( We delete every cache file while we're logged in and working on our website, this is to ensure that all cache files are up-to-date )
+
+
+// SESSION BASED CHECK 
+// When a user is logged, we delete every cache file, this is to ensure that all cache files are up-to-date )
+// Pages with a session message are not cached too, to avoid caching the message itself
 
 if (!empty($_SESSION['Auth']['User'])) {
     $debugInfo = "Authenticated, emptying cache files";
     $cache = 0;
 
-    // Removing all cache files and directories
+    // Remove all cache files and directories
     $contents = new RecursiveDirectoryIterator( $cachePath, FilesystemIterator::SKIP_DOTS );
-    $contents = new RecursiveIteratorIterator( $content, RecursiveIteratorIterator::CHILD_FIRST );
+    $contents = new RecursiveIteratorIterator( $contents, RecursiveIteratorIterator::CHILD_FIRST );
     foreach ( $contents as $content ) {
         $content->isDir() ?  rmdir($content) : unlink($content);
     }
 }
-
+if (!empty($_SESSION['Message'])) {
+    $debugInfo = "There's a session message to show, no caching :)";
+    $cache = 0;
+}
 
 
 /*
@@ -88,7 +128,8 @@ if ($cache == 0) { // No caching, because either configuration or one of the che
     $cachePath = $cachePath . $_SERVER["REQUEST_URI"];
     $cacheFile = $cachePath . '/index.html';
     
-    if (is_readable($cacheFile)) { // If a cache file is already there, let's use it. CakePHP can rest for a while, this time :)
+    // If a cache file is already there, let's use it. CakePHP can rest for a while, this time :)
+    if (is_readable($cacheFile)) { 
         $debugInfo = "Showing cached file: $cacheFile";
 
         ob_start();
@@ -96,7 +137,8 @@ if ($cache == 0) { // No caching, because either configuration or one of the che
         $page = ob_get_clean();
         echo $page;
 
-    } else { // This page is not cached. Let's create a new one. Sorry Cake :)
+    // This page is not cached. Let's create a new one. Sorry Cake :)
+    } else { 
         $debugInfo = "Creating a new cache file...";
 
         ob_start();
